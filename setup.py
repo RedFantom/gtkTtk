5d128562b6f193a48749fbdbb49c9fa1b4976d03
RedFantom <redfantom@outlook.com>
2020-12-13 12:32:13 +0100

    Add dependency walking for DLL file finding

--- setup.py
+++ setup.py
@@ -41,8 +41,11 @@ if "linux" in sys.platform:
     kwargs = {"install_requires": ["scikit-build"], "cmdClass": {"build": BuildCommand}}
 
 elif "win" in sys.platform:
+    import os
     import shutil
     from setuptools import setup
+    import subprocess as sp
+    from typing import List, Optional
 
     print("Running setup on Windows: Assuming that libgttk.dll has been built and is in working directory")
     print("If libgttk.dll has not been built: It should be built with MSYS")
@@ -50,7 +53,79 @@ elif "win" in sys.platform:
     shutil.copy("library\\gttk.tcl", "gttk\\gttk.tcl")
     shutil.copy("library\\pkgIndex.tcl", "gttk\\pkgIndex.tcl")
 
-    kwargs = {"package_data": {"gttk": ["libgttk.dll", "pkgIndex.tcl", "gttk.tcl"]}}
+
+    class DependencyWalker(object):
+        """
+        Walk the dependencies of a DLL file and find all DLL files
+
+        DLL files are searched for in all the directories specified by
+        - The PATH environment variable
+        - The DLL_SEARCH_PATHS environment variable
+        """
+
+        SYSTEM_DLLS = ["KERNEL32.dll", "MSVCRT.dll"]
+
+        def __init__(self, dll_file: str, dependencies_exe="./deps/dependencies.exe"):
+            if not os.path.exists(dependencies_exe):
+                print("dependencies.exe is required to find all dependency DLLs")
+                raise FileNotFoundError("Invalid path specified for dependencies.exe")
+            self._exe = dependencies_exe
+            if not os.path.exists(dll_file):
+                raise FileNotFoundError("'{}' does not specify a valid path to first file".format(dll_file))
+            self._dll_file = dll_file
+
+        @property
+        def dependency_dll_files(self) -> List[str]:
+            """Return a list of abspaths to the dependency DLL files"""
+            print("Walking dependencies of {}".format(self._dll_file))
+            dlls = [self._dll_file]
+            done = []
+            while set(dlls) != set(done):  # As long as not all dlls are done, keep searching
+                for dll in set(dlls) - set(done):  # Go only over not-yet done DLLs
+                    p = sp.Popen([self._exe, "-import", dll], stdout=sp.PIPE)
+                    stdout, stderr = p.communicate()
+                    new_dlls = self._parse_dependencies_output(stdout)
+                    for new_dll in new_dlls:
+                        if new_dll in self.SYSTEM_DLLS:
+                            continue  # Exclude any DLLs that are known to be from the system
+                        p = self._find_dll_abs_path(new_dll)
+                        if p is None:
+                            print("DLL file {} could not be located!".format(new_dll))
+                        elif p not in dlls:
+                            dlls.append(p)
+                    done.append(dll)
+            return list(set(dlls))
+
+        @staticmethod
+        def _parse_dependencies_output(output: str) -> List[str]:
+            """Parse the output of the dependencies.exe command"""
+            dlls: List[str] = list()
+            for line in map(str.strip, output.split("\n")):
+                if not line.startswith("Import from module"):
+                    continue
+                line = line.strip("Import from module").strip(":").strip()
+                dlls.append(line)
+            return dlls
+
+        @staticmethod
+        def _find_dll_abs_path(dll_name: str) -> Optional[str]:
+            """Find the absolute path of a specific DLL file specified"""
+            for var in ("PATH", "DLL_SEARCH_DIRECTORIES"):
+                val = os.environ.get(var, "")
+                for dir in val.split(";"):
+                    if not os.path.exists(dir) and os.path.isdir(dir):
+                        print("'{}' found as specified path but not a valid directory".format(dir))
+                        continue
+                    for dirpath, subdirs, files in os.walk(dir):
+                        if dll_name in files:
+                            return os.path.join(dirpath, dll_name)
+            return None
+
+
+    for p in DependencyWalker("libgttk.dll").dependency_dll_files:
+        shutil.copyfile(p, os.path.join("gttk", os.path.basename(p)))
+
+    kwargs = {"package_data": {"gttk": ["*.dll", "pkgIndex.tcl", "gttk.tcl"]}}
 
 else:
     print("Only Linux and Windows are currently supported by the build system")
